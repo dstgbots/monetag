214dea4592e0fad991a32ea7d5f69d311339c6ee
dstgbots <138157172+dstgbots@users.noreply.github.com>
2025-01-01 22:43:35 +0530

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,8 @@
+import sys, os
+INTERP = os.path.join(os.getcwd(), 'venv', 'bin', 'python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+from app import create_app
+application = create_app()
 
