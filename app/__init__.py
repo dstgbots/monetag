90134699606cd0365471bb057377eff71e914c68
dstgbots <138157172+dstgbots@users.noreply.github.com>
2025-01-01 22:42:05 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from config.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    from app.routes import api_bp
+    app.register_blueprint(api_bp)
+    
+    return app
 
